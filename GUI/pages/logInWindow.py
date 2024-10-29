2270380951b015176d22875504d4ba892d84b070
GitSchwan <fabianschwanck@hotmail.com>
2024-10-29 15:51:28 +0100

    Add search bar, search button, and filter button to mainPage layout

--- GUI/pages/logInWindow.py
+++ GUI/pages/logInWindow.py
@@ -14,7 +14,7 @@ class logInWindow(tk.Frame):
 		tk.Frame.__init__(self, parent)
 
 		defaultbg = self.cget('bg')
-
+		self.configure(background="white")
 
 		# Funktion für den Login-Button (Platzhalter)
 		def logIn():
@@ -48,24 +48,24 @@ class logInWindow(tk.Frame):
 		greyFrame.grid_columnconfigure(0, weight=1)
 
 		# Erstelle das Login-Formular im mittleren Bereich
-		loginFrame = tk.Frame(self)
+		loginFrame = tk.Frame(self, bg="white")
 		loginFrame.grid(row=1, column=0, sticky=tk.N + tk.S, pady=100)
 
 		# Füge die Eingabefelder für Username und Passwort hinzu
-		username_label = ttk.Label(loginFrame, text="Benutzername", font=LOGINFONT)
-		username_label.grid(column=0, row=1, sticky=tk.W + tk.E, padx=130, pady=5)
+		usernameLabel = ttk.Label(loginFrame, text="Benutzername", font=LOGINFONT, background="white")
+		usernameLabel.grid(column=0, row=1, sticky=tk.W + tk.E, padx=130, pady=5)
 
-		username_entry = tk.Entry(loginFrame, font=LOGINFONT)
-		username_entry.grid(column=0, row=2, sticky=tk.W + tk.E, padx=5, pady=5)
+		usernameEntry = tk.Entry(loginFrame, bg=defaultbg, font=LOGINFONT, bd=1)
+		usernameEntry.grid(column=0, row=2, sticky=tk.W + tk.E, padx=5, pady=5)
 
-		password_label = ttk.Label(loginFrame, text="Passwort", font=LOGINFONT)
-		password_label.grid(column=0, row=3, sticky=tk.W + tk.E, padx=190, pady=5)
+		passwordLabel = ttk.Label(loginFrame, text="Passwort", font=LOGINFONT, background="white")
+		passwordLabel.grid(column=0, row=3, sticky=tk.W + tk.E, padx=190, pady=5)
 
-		password_entry = tk.Entry(loginFrame, show="*", font=LOGINFONT)
-		password_entry.grid(column=0, row=4, sticky=tk.W + tk.E, padx=5, pady=5)
+		passwordEntry = tk.Entry(loginFrame, show="*", bg=defaultbg, font=LOGINFONT, bd=1)
+		passwordEntry.grid(column=0, row=4, sticky=tk.W + tk.E, padx=5, pady=5)
 
 		self.logOutBtn = tk.PhotoImage(file="assets/Anmelden.png")
 
 		# Login-Button, der zur Hauptseite führt
-		login_button = tk.Button(loginFrame, image=self.logOutBtn,bg=defaultbg, command=logIn, font=LOGINFONT,  bd=0, relief=tk.FLAT, activebackground=defaultbg)
-		login_button.grid(column=0, row=5, sticky=tk.W + tk.E, padx=5, pady=30)
+		loginButton = tk.Button(loginFrame, image=self.logOutBtn,bg="white", command=logIn, font=LOGINFONT,  bd=0, relief=tk.FLAT, activebackground="white")
+		loginButton.grid(column=0, row=5, sticky=tk.W + tk.E, padx=5, pady=30)
--- GUI/pages/mainPage.py
+++ GUI/pages/mainPage.py
@@ -14,11 +14,21 @@ class mainPage(tk.Frame):
 
     def __init__(self, parent, controller):
         tk.Frame.__init__(self, parent)
+        self.configure(background="white")
+
+        # Funktion für den Logout-Button
+        def logOut():
+            from .logInWindow import logInWindow
+            controller.show_frame(logInWindow)  # funktionalität hinzufügen
+
+        def search():
+            print("I am Searching")
 
         # Konfiguriere das Grid-Layout für die Hauptseite
         self.grid_rowconfigure(0, weight=0)
         self.grid_rowconfigure(1, weight=1)
-        self.grid_rowconfigure(2, weight=1)
+        self.grid_rowconfigure(2, weight=0)
+        self.grid_rowconfigure(3, weight=0)
         self.grid_columnconfigure(0, weight=1)
 
         # Erstelle einen Header-Bereich
@@ -27,21 +37,14 @@ class mainPage(tk.Frame):
 
         # Konfiguriere die Spalten für den Header
         headerFrame.grid_columnconfigure(0, weight=1)
-        headerFrame.grid_columnconfigure(1, weight=0)
-        headerFrame.grid_columnconfigure(2, weight=1)
-        headerFrame.grid_columnconfigure(3, weight=0)
         headerFrame.grid_rowconfigure(0, weight=1)
 
-        self.srhHead = tk.PhotoImage(file="assets/srhHeader.png")
+        self.srhHead = tk.PhotoImage(file="assets/srh.png")
 
         # Füge ein zentriertes Label hinzu
         headerLabel = tk.Label(headerFrame, image=self.srhHead, background="#DF4807", foreground="white")
         headerLabel.grid(row=0, column=0, padx=20, pady=20, sticky=tk.N + tk.W)
 
-        # Funktion für den Logout-Button
-        def logOut():
-            from .logInWindow import logInWindow
-            controller.show_frame(logInWindow)  # funktionalität hinzufügen
 
         # Konvertiere das Bild für Tkinter
         self.logOutBtn = tk.PhotoImage(file="assets/ausloggen.png")
@@ -51,7 +54,45 @@ class mainPage(tk.Frame):
                                  activebackground="#DF4807")
         logOutButton.grid(row=0, column=3, sticky=tk.E, padx=20)
 
-        treeFrame = tk.Frame(self, background='white')
+
+
+        greyFrame = tk.Frame(self, height=10, background="#F4EFEF")
+        greyFrame.grid(row=1, column=0, sticky=tk.W + tk.E + tk.N)
+
+        # Füge den LogIn-Label zur Frame hinzu
+        logInLabel = tk.Label(greyFrame, text="Inventurübersicht", bd=0, relief=tk.FLAT, bg="#F4EFEF", font=("Arial", 20))
+        logInLabel.grid(padx=200, pady=5, row=0, column=0, sticky=tk.W)
+
+        # Konfiguriere den greyFrame für zentrierte Ausrichtung
+        greyFrame.grid_columnconfigure(0, weight=1)
+
+        greyFrameSide = tk.Frame(self, height=10, background=srhGrey)
+        greyFrameSide.grid(row=1, column=0, sticky=tk.W + tk.N + tk.S)
+
+        overviewLabel = tk.Label(greyFrameSide, text="Räume", bd=0, relief=tk.FLAT, bg=srhGrey, font=("Arial", 20))
+        overviewLabel.grid(padx=40, pady=5, row=0, column=0, sticky=tk.W + tk.E)
+
+        # Verschiebe den SearchFrame nach oben, indem du seine Zeile anpasst
+        searchFrame = tk.Frame(self,bg="white")
+        searchFrame.grid(pady=50, padx=200,row=1, column=0, sticky=tk.W + tk.E + tk.N)
+
+        searchFrame.grid_columnconfigure(0, weight=0)
+        searchFrame.grid_columnconfigure(1, weight=1)
+        searchFrame.grid_columnconfigure(2, weight=0)
+
+        self.searchBtn = tk.PhotoImage(file="assets/SearchButton.png")
+        searchButton = tk.Button(searchFrame, image=self.searchBtn, bd=0, relief=tk.FLAT, bg="white", activebackground="white", command=search)
+        searchButton.grid(padx=20, pady=5, row=0, column=0)
+
+        searchEntry = tk.Entry(searchFrame, bg="white",background="#F4EFEF", font=("Arial", 20), bd=0)
+        searchEntry.grid(column=1, row=0, columnspan=2, sticky=tk.W + tk.E, padx=5, pady=5)
+
+        self.filterBtn = tk.PhotoImage(file="assets/Filter.png")
+        filterButton = tk.Button(searchFrame, image=self.filterBtn, bd=0, relief=tk.FLAT, bg="white", activebackground="white")
+        filterButton.grid(row=0, column=3, padx=10)
+
+        # Ändere die Position des TreeFrames auf row=3
+        treeFrame = tk.Frame(self, background=srhGrey)
         treeFrame.grid(row=1, column=0)
 
         tree = ttk.Treeview(treeFrame, column=("c1", "c2", "c3", "c4", "c5"), show="headings", height=5)
