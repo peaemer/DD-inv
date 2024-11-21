58c3d6a1b41b30744259898297078d25b77c6e3b
GitSchwan <fabianschwanck@hotmail.com>
2024-11-21 10:45:55 +0100

    work rdy settingsWindow.py for chautoo

--- GUI/pages/__init__.py
+++ GUI/pages/__init__.py
@@ -1,6 +1,7 @@
 from .logInWindow import logInWindow
 from .mainPage import mainPage
 from .addItemPopup import addItemPopup
+from .settingsWindow import popUpSettings
 
 
-__all__ = ["logInWindow", "mainPage", "addItemPopup"]
+__all__ = ["logInWindow", "mainPage", "addItemPopup","settingsWindow"]
