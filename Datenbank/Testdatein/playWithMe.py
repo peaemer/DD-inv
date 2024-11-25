971a4cbaedf8c0fec83905e683a80323beb7530e
Jack <Jack-Mike.Saering@srhk.de>
2024-11-25 09:00:55 +0100

    API Version 2.0 -Exceptions are handled with e.args and RuntimeErrors now -Create_rolle will now be functionable and is used as loop to shorten the code -with funktion is used not to work more fluent with db connections

--- Datenbank/Testdatein/playWithMe.py
+++ Datenbank/Testdatein/playWithMe.py
@@ -1,8 +1,55 @@
 from Datenbank.sqlite3api import *
 
-def create_self():
+def create_neue_benutzer():
     print("-------Benutzer wird erstellt-------")
-    print(create_benutzer("Ralf","#Ralf123","Ralf@srhk.de"))
+    print(create_benutzer("Anna", "#Anna456", "Anna@srhk.de"))
+
+    print("-------Benutzer wird erstellt-------")
+    print(create_benutzer("Max", "#Max789", "Max@srhk.de"))
+
+    print("-------Benutzer wird erstellt-------")
+    print(create_benutzer("Lisa", "#Lisa321", "Lisa@srhk.de"))
+
+    print("-------Benutzer wird erstellt-------")
+    print(create_benutzer("Tom", "#Tom654", "Tom@srhk.de"))
+
+    print("-------Benutzer wird erstellt-------")
+    print(create_benutzer("Sophia", "#Sophia987", "Sophia@srhk.de"))
+
+    print("-------Benutzer wird erstellt-------")
+    print(create_benutzer("Lukas", "#Lukas112", "Lukas@srhk.de"))
+
+    print("-------Benutzer wird erstellt-------")
+    print(create_benutzer("Mia", "#Mia223", "Mia@srhk.de"))
+
+    print("-------Benutzer wird erstellt-------")
+    print(create_benutzer("Jonas", "#Jonas334", "Jonas@srhk.de"))
+
+    print("-------Benutzer wird erstellt-------")
+    print(create_benutzer("Clara", "#Clara445", "Clara@srhk.de"))
+
+    print("-------Benutzer wird erstellt-------")
+    print(create_benutzer("Felix", "#Felix556", "Felix@srhk.de"))
+
+def create_neue_rolle():
+    # Test für das Erstellen einer Rolle
+    print("\n--- Test: Rolle erstellen ---")
+    print(create_rolle(
+        Rolle="Test-Rolle",
+        ANSEHEN="True",
+        ROLLE_LOESCHBAR="True",
+        ADMIN_FEATURE="True",
+        LOESCHEN="True",
+        BEARBEITEN="True",
+        ERSTELLEN="True",
+        GRUPPEN_LOESCHEN="True",
+        GRUPPEN_ERSTELLEN="True",
+        GRUPPEN_BEARBEITEN="True",
+        ROLLEN_ERSTELLEN="True",
+        ROLLEN_BEARBEITEN="True",
+        ROLLEN_LOESCHEN="True"
+    ))
+
 
 if __name__ == '__main__':
-    create_self()
+    create_neue_rolle()
