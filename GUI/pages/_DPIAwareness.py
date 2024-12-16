41455b5f30083d16a6030ddf7b1991df8614e85b
Jack <Jack-Mike.Saering@srhk.de>
2024-12-16 14:56:40 +0100

    Remove unused files and assets from the project refactored the code
    This commit deletes unnecessary files, including old resources, fonts, icons, and Python scripts, to clean up the repository. It streamlines the project structure by removing redundant or obsolete items, improving maintainability.

--- GUI/pages/_DPIAwareness.py
+++
@@ -1,7 +0,0 @@
-# Richtiges Skalieren der Schrift bei hoeherer Bildschirmaufloesung
-# (e.g. FHD, WQHD, UHD, etc. [only for Windows!!])
-try:
-    from ctypes import windll
-    windll.shcore.SetProcessDpiAwareness(1)
-except:
-    pass
