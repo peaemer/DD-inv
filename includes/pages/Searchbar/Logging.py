ea45370b500dd8b5e2d867ad2696eb22cac654bd
Alex5X5 <alex.25.lehnert@gmail.com>
2025-01-15 11:04:35 +0100

    Added a simple Logging Module. Searchbar2.py and SearchbarLogic.py now use the logger instead of manual printing.

---
+++ includes/pages/Searchbar/Logging.py
@@ -0,0 +1,34 @@
+from copy import copy
+from typing import Final
+
+message_type_style:Final[str] = '\033[1;32m'
+class_style:Final[str] = '\033[1;34m'
+reset_style:Final[str] = '\033[0;0m'
+
+DEBUG_MODE_NORMAL:Final[bool] = True
+DEBUG_MODE_ALL:Final[bool] = True
+
+
+class Logger:
+
+    levels:list[str]
+
+    def __init__(self, level:str, levels:list[str]=None):
+        self.levels:list[str] = []
+        if levels:
+            self.levels = copy(levels)
+        self.levels.append(level)
+
+    def debug(self, message:str):
+        if DEBUG_MODE_NORMAL is True:
+            levels:str = ''
+            for level in self.levels:
+                levels = levels + f'[{level}]'
+            print(f'{message_type_style}[Debug]{class_style}{levels}:{reset_style}' + message)
+
+    def debug_e(self, message:str):
+        if DEBUG_MODE_ALL is True:
+            levels:str = ''
+            for level in self.levels:
+                levels = levels + f'[{level}]'
+            print(f'{message_type_style}[Debug]{class_style}{levels}:{reset_style}' + message)
