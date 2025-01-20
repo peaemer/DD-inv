aa422a1b94f4d5d72179350e98c69dabf2102795
GitSchwan <fabianschwanck@hotmail.com>
2025-01-20 14:40:46 +0100

    added docstring for new function in _sort_tree.py

--- includes/pages/_sort_tree.py
+++ includes/pages/_sort_tree.py
@@ -1,6 +1,33 @@
 from tkinter import ttk
 
 def sort_column(tree_name: ttk.Treeview, col, reverse=False):
+    """
+    Sortiert die Einträge einer Spalte in einer `ttk.Treeview`-Tabelle.
+
+    Diese Funktion sortiert die Inhalte der angegebenen Spalte entweder numerisch oder alphanumerisch,
+    abhängig vom Datentyp der Spaltenwerte. Zusätzlich wird die Reihenfolge der Einträge im Treeview
+    aktualisiert, und die Tags für "oddrow" (ungerade Zeilen) und "evenrow" (gerade Zeilen) werden
+    entsprechend neu gesetzt. Der Header der Spalte wird so konfiguriert, dass ein Klick auf den Header
+    die Sortierrichtung umkehrt.
+
+    Args:
+        tree_name (ttk.Treeview): Die Treeview-Instanz, deren Spalte sortiert werden soll.
+        col (str): Der Name der zu sortierenden Spalte.
+        reverse (bool, optional): Gibt an, ob die Sortierung in umgekehrter Reihenfolge erfolgen soll.
+            Standardmäßig False für aufsteigende Sortierung.
+
+    Raises:
+        ValueError: Falls beim Überprüfen von numerischen Werten ein unerwarteter Typ auftritt.
+
+    Notes:
+        - Die Funktion überprüft, ob alle Werte in der Spalte numerisch sind (sofern nicht leer)
+          und wählt basierend darauf die geeignete Sortierlogik (numerisch oder alphanumerisch).
+        - Nach der Sortierung werden die Tags für "oddrow" und "evenrow" neu gesetzt, um ein visuelles
+          Unterscheiden der Zeilen zu ermöglichen.
+        - Die Funktion modifiziert den Header der Spalte, sodass beim nächsten Klick die Sortierrichtung
+          umgekehrt wird.
+    """
+
     # Daten aus der Treeview abrufen
     data = [(tree_name.set(item, col), item) for item in tree_name.get_children('')]
 
