bb1851d7a1e91158e8cd7067321807ecc7e1c868
Alex5X5 <alex.25.lehnert@gmail.com>
2025-02-04 13:25:19 +0100

    added more characters to allowed characters inside Searchbar

--- includes/pages/Searchbar/Searchbar2.py
+++ includes/pages/Searchbar/Searchbar2.py
@@ -12,7 +12,7 @@ from .SearchbarLogic import start_search, update_search, finish_search, get_most
 ALLOWED_CHARACTERS: Final[list[str]] = [
     'a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm', 'n', 'o', 'p', 'q', 'r', 's', 't', 'u', 'v', 'w', 'x', 'y', 'z',
     'A', 'B', 'C', 'D', 'E', 'F', 'G', 'H', 'I', 'J', 'K', 'L', 'M', 'N', 'O', 'P', 'Q', 'R', 'S', 'T', 'U', 'V', 'W', 'X', 'Y', 'Z',
-    '0', '1', '2', '3', '4', '5', '6', '7', '8', '9', ' ', '_', ',', '.'
+    '0', '1', '2', '3', '4', '5', '6', '7', '8', '9', ' ', '-', '_', '~', ',', '.', ';', ':', '§', '$', '%', '&', '?', '=', '`', '*', '#', '/', '(', ')', '{', '}', '<', '>', '|', '°'
 ]
 
 STOPS_FAST_MARKING: Final[list[str]] = [
