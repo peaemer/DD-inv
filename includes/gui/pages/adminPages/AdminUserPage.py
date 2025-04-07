d7dd23d00a0d2a4096b7e9160fa24f2fa0792db8
Alex5X5 <alex.25.lehnert@gmail.com>
2025-04-07 14:05:24 +0200

    cleaned up directories and deprecated files

--- includes/gui/pages/adminPages/AdminUserPage.py
+++ includes/gui/pages/adminPages/AdminUserPage.py
@@ -13,7 +13,7 @@ logger:Logger = Logger('AdminUserPage')
 class AdminUserPage(AdminPage):
     @override
     def on_cell_click(self, cell_text:str) -> None:
-        pass
+        print('test')
 
     def __init__(self, parent, controller):
         super().__init__(
@@ -23,7 +23,7 @@ class AdminUserPage(AdminPage):
             window_name='dd inv',
             add_button_callback=lambda :AddUserPopup.AddUserPopup(self.winfo_toplevel()),
             get_data_callback=sqlite3api.read_all_benutzer,
-            select_item_callback=UserDetailsWindow.show_user_details(),
+            select_item_callback=None,
             tree_structure={'Name': 100, 'Passwort': 200, 'E-Mail': 300, 'Rolle': 100}
         )
         self.apply_layout()
